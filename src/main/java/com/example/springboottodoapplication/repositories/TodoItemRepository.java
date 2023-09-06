4b8bc49ea1a4c98929aabdf5c9f86662ab203c94
stanley <cjstanenemuo@yahoo.com>
2023-09-06 02:49:03 -0400

    added new packages

---
+++ src/main/java/com/example/springboottodoapplication/repositories/TodoItemRepository.java
@@ -0,0 +1,10 @@
+package com.example.springboottodoapplication.repositories;
+
+import com.example.springboottodoapplication.models.TodoItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+
+}
