e377e6f66c37e6bf5200f1b37ef5ad70a4bab2f8
stanley <cjstanenemuo@yahoo.com>
2023-09-06 03:05:52 -0400

    updated HomeController.java packages

--- src/main/java/controllers/HomeController.java
+++ src/main/java/controllers/HomeController.java
@@ -1,5 +1,20 @@
 package controllers;
 
+import org.springframework.stereotype.Controller;
+import com.example.springboottodoapplication.services.TodoItemService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
 public class HomeController {
     
+    @Autowired
+    private TodoItemService todoItemService;
+
+    @GetMapping("/")
+    public ModelAndView index() {
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("todoItems", todoItemService.getAll());
+        return modelAndView;
+    }
 }
